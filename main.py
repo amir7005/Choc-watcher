badb2716316fa72e92cae571f21f6d5695d5e09b
AmirMahdi <168308022+amir7005@users.noreply.github.com>
2025-07-26 15:50:07 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 app = Flask(__name__)
 
 # اطلاعات ربات و چت آیدی — حتماً اینجا توکن جدید رو بذار
-BOT_TOKEN = 'توکن_جدید_تو_اینجا'
+BOT_TOKEN = '8253237534:AAFZr4EpriINZtYsuKB2EY4sO7S8Ja52Jhc'
 CHAT_ID = '5214257544'
 
 def send_test_message():
